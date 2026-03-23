dc3f2c9d8a88af8fe959f837464c28d4fa362582
KimJayhyun <dnlwjem000@gmail.com>
2026-03-23 15:46:05 +0900

    init commit

---
+++ src/test/java/com/example/batch/batch_system/BatchSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.batch.batch_system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BatchSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
